c402fe776b9a96cae6e82b8c4ab3a3b2c427c099
zee william <william.zee@edu.esiee.fr>
2026-01-19 21:46:11 +0100

    Fix: final source paths for modules

--- td5/scripts/tofu/live/lambda-sample/main.tf
+++ td5/scripts/tofu/live/lambda-sample/main.tf
@@ -3,27 +3,19 @@ provider "aws" {
 }
 
 module "function" {
-  # On pointe vers TON dépôt
   source = "github.com/william-zee/devops_base//td5/scripts/tofu/modules/lambda"
-
-  name = "lambda-sample"
-  src  = "${path.module}/src"
+  name   = "lambda-sample"
+  src    = "${path.module}/src"
 }
-
 module "gateway" {
-  # On pointe vers TON dépôt
   source = "github.com/william-zee/devops_base//td5/scripts/tofu/modules/api-gateway"
-
   name                = "lambda-sample"
   function_arn        = module.function.function_arn
   function_name       = module.function.function_name
   api_gateway_route_key = "GET /"
 }
-
-# Ce module sert pour les tests automatisés du pipeline
 module "test" {
   source = "github.com/william-zee/devops_base//td5/scripts/tofu/modules/test-endpoint"
-
   endpoint = module.gateway.api_endpoint
 }
 
