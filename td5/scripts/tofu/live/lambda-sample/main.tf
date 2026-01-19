bb3691ef3778eb45bfdc2c769432f59c6fb765c2
zee william <william.zee@edu.esiee.fr>
2026-01-19 22:28:14 +0100

    Fix: add missing function_arn to gateway module

--- td5/scripts/tofu/live/lambda-sample/main.tf
+++ td5/scripts/tofu/live/lambda-sample/main.tf
@@ -12,14 +12,15 @@ module "function" {
   timeout     = 5
 }
 module "gateway" {
-  source = "../../modules/api-gateway"
-  name   = "lambda-sample"
+  source       = "../../modules/api-gateway"
+  name         = "lambda-sample"
+  function_arn = module.function.function_arn
   api_gateway_routes = {
     "GET /" = module.function.function_arn
   }
 }
 module "test" {
-  source = "../../modules/test-endpoint"
+  source   = "../../modules/test-endpoint"
   endpoint = module.gateway.api_endpoint
 }
 
