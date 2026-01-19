fc2015c4f69dbe73b37fba0b489e9b70d7deaa06
zee william <william.zee@edu.esiee.fr>
2026-01-19 20:32:33 +0100

    Ajout du module state-bucket officiel

--- td5/scripts/tofu/modules/state-bucket/main.tf
+++ td5/scripts/tofu/modules/state-bucket/main.tf
@@ -1,34 +1,40 @@
-variable "name" {
-  description = "Nom du bucket et de la table"
-  type        = string
-}
-
-resource "aws_s3_bucket" "terraform_state" {
+resource "aws_s3_bucket" "tofu_state" {
   bucket        = var.name
   force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "enabled" {
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.tofu_state.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.tofu_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.tofu_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "tofu_locks" {
   name         = var.name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
+
   attribute {
     name = "LockID"
     type = "S"
   }
-}
-
-output "bucket_name" {
-  value = aws_s3_bucket.terraform_state.bucket
-}
-
-output "dynamodb_table_name" {
-  value = aws_dynamodb_table.terraform_locks.name
 }
